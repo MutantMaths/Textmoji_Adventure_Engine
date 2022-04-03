5a24dcf91dbd47fd33ab6a3d94716d8825924e64
Jonathan Haywood <102903525+MutantMaths@users.noreply.github.com>
2022-04-03 11:00:45 +0000

    colors

--- color.py
+++ color.py
@@ -1,19 +1,19 @@
 none = "\x1b[0m"
 
-red_bg = "\x1b[6;30;41m"
-green_bg = "\x1b[6;30;42m"
-yellow_bg = "\x1b[6;30;43m"
-light_blue_bg = "\x1b[6;30;44m"
-purple_bg = "\x1b[6;30;45m"
-blue_bg = "\x1b[6;30;46m"
-white_bg = "\x1b[6;30;47m"
-white_fg_green_bg = "\x1b[6;37;42m"
-white_fg_blue_bg = "\x1b[6;37;46m"
+red_bg = "\x1b[0;30;41m"
+green_bg = "\x1b[0;30;42m"
+yellow_bg = "\x1b[0;30;43m"
+light_blue_bg = "\x1b[0;30;44m"
+purple_bg = "\x1b[0;30;45m"
+blue_bg = "\x1b[0;30;46m"
+white_bg = "\x1b[0;30;47m"
+white_fg_green_bg = "\x1b[0;37;42m"
+white_fg_blue_bg = "\x1b[0;37;46m"
 
-red_fg = "\x1b[6;31;40m"
-yellow_fg = "\x1b[6;33;40m"
-blue_fg = "\x1b[6;34;40m"
-purple_fg = "\x1b[6;35;40m"
+red_fg = "\x1b[0;31;40m"
+yellow_fg = "\x1b[0;33;40m"
+blue_fg = "\x1b[0;34;40m"
+purple_fg = "\x1b[0;35;40m"
 
 item = red_fg
 npc = blue_fg
